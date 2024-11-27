94edf43d5239ca1511b1bb3c41d450c21d7fc75d
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-11-27 14:36:55 +0000

    add projectile

--- config/packages.el
+++ config/packages.el
@@ -1,7 +1,12 @@
 ;;; packages.el - Configuration for packages
 
 ;; company
-;; company
 (use-package company
   :config
   (global-company-mode))
+
+;; projectile
+(use-package projectile
+  :config
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map))
--- init.el
+++ init.el
@@ -7,8 +7,8 @@
 
 ;; config files
 (load-file "~/.emacs.d/config/keys.el") ; Loading key-bindings
-(load-file "~/.emacs.d/config/appearance.el") ; Loading custom appearance stuff
 (load-file "~/.emacs.d/config/packages.el") ; Loading custom package configuration
+(load-file "~/.emacs.d/config/appearance.el") ; Loading custom appearance stuff
 
 ;; melpa
 (require 'package)
@@ -20,7 +20,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company magit)))
+ '(package-selected-packages '(projectile company magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
