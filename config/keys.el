a4d8a4e70681997a9932795c40542f8937ec0e14
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-11-27 15:21:57 +0000

    add new bind

--- config/keys.el
+++ config/keys.el
@@ -1,11 +1,16 @@
 ;;; keys.el --- Custom keybindings
 
+;;; Commentary:
+
+;;; Code:
+
 ;; misc
 (global-set-key (kbd "s-w") 'kill-buffer-and-window) ; Cmd+w - close buffer and window
 (global-set-key (kbd "s-d") 'dired) ; Cmd+d - dired
 (global-set-key (kbd "s-<return>") 'eval-buffer) ; Cmd+return - eval buffer
 (global-set-key (kbd "s-b") 'ibuffer) ; Cmd+b - ibuffer
 (global-set-key (kbd "s-o") 'find-file) ; Cmd+o - find file
+(global-set-key (kbd "s-k") 'kill-whole-line) ; Cmd+k - kill whole line
 
 ;; magit
 (define-prefix-command 'magit-kb-map)
@@ -19,3 +24,7 @@
 (global-set-key (kbd "s-<down>") 'windmove-down) ; Cmd++down, Move down buffer
 (global-set-key (kbd "M-s-<right>") 'next-buffer) ; Cmd+lt+right-arrow, moves to next buffer
 (global-set-key (kbd "M-s-<left>") 'previous-buffer) ; Cmd+alt+left-arrow, moves to previous buffer
+
+(provide 'keys)
+
+;;; keys.el ends here
