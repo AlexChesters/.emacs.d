8a815343a5537c7b27886b0b340c80f86d7c4f8d
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-11-27 15:04:42 +0000

    add flycheck

--- config/packages.el
+++ config/packages.el
@@ -1,12 +1,25 @@
-;;; packages.el - Configuration for packages
+;;; packages.el --- Configuration for packages
+
+;;; Commentary:
+
+;;; Code:
 
 ;; company
 (use-package company
   :config
   (global-company-mode))
 
+;; flycheck
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
 ;; projectile
 (use-package projectile
   :config
   (projectile-mode +1)
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map))
+
+(provide 'packages)
+
+;;; packages.el ends here
