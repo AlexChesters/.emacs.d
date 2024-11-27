4a3c4a622a3ae44f0c4389a05839a9a67f2321d2
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-11-27 13:59:15 +0000

    add new binds

--- config/keys.el
+++ config/keys.el
@@ -3,8 +3,17 @@
 ;; misc
 (global-set-key (kbd "s-w") 'kill-buffer-and-window) ; Cmd+w - close buffer and window
 (global-set-key (kbd "s-d") 'dired) ; Cmd+d - dired
+(global-set-key (kbd "s-<return>") 'eval-buffer) ; Cmd+return - eval buffer
 
 ;; magit
 (define-prefix-command 'magit-kb-map)
 (global-set-key (kbd "C-c v") 'magit-kb-map)
 (define-key 'magit-kb-map (kbd "s") 'magit-status)
+
+;; navigation
+(global-set-key (kbd "s-<left>") 'windmove-left) ; Cmd+left, Move left buffer
+(global-set-key (kbd "s-<right>") 'windmove-right) ; Cmd+right, Move right buffer
+(global-set-key (kbd "s-<up>") 'windmove-up) ; Cmd++up, Move up buffer
+(global-set-key (kbd "s-<down>") 'windmove-down) ; Cmd++down, Move down buffer
+(global-set-key (kbd "M-s-<right>") 'next-buffer) ; Cmd+lt+right-arrow, moves to next buffer
+(global-set-key (kbd "M-s-<left>") 'previous-buffer) ; Cmd+alt+left-arrow, moves to previous buffer
