37c79c0716f402ef1042db1a2aa6fea5a7153897
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-11-28 10:24:02 +0000

    change bindings

--- config/keys.el
+++ config/keys.el
@@ -21,12 +21,12 @@
 (define-key 'magit-kb-map (kbd "s") 'magit-status)
 
 ;; navigation
-(global-set-key (kbd "s-<left>") 'windmove-left) ; Cmd+left, Move left buffer
-(global-set-key (kbd "s-<right>") 'windmove-right) ; Cmd+right, Move right buffer
-(global-set-key (kbd "s-<up>") 'windmove-up) ; Cmd++up, Move up buffer
-(global-set-key (kbd "s-<down>") 'windmove-down) ; Cmd++down, Move down buffer
-(global-set-key (kbd "M-s-<right>") 'next-buffer) ; Cmd+lt+right-arrow, moves to next buffer
-(global-set-key (kbd "M-s-<left>") 'previous-buffer) ; Cmd+alt+left-arrow, moves to previous buffer
+(global-set-key (kbd "M-s-<left>") 'windmove-left) ; Cmd+alt+left, Move left buffer
+(global-set-key (kbd "M-s-<right>") 'windmove-right) ; Cmd+alt+right, Move right buffer
+(global-set-key (kbd "M-s-<up>") 'windmove-up) ; Cmd+alt+up, Move up buffer
+(global-set-key (kbd "M-s-<down>") 'windmove-down) ; Cmd+alt+down, Move down buffer
+(global-set-key (kbd "s-<right>") 'next-buffer) ; Cmd+right-arrow, moves to next buffer
+(global-set-key (kbd "s-<left>") 'previous-buffer) ; Cmd+left-arrow, moves to previous buffer
 
 (provide 'keys)
 
