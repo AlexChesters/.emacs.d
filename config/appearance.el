dbd9d9c41cf7886c7b22a39457468f1b1844359d
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-11-28 17:24:17 +0000

    splitting

--- config/appearance.el
+++ config/appearance.el
@@ -20,6 +20,10 @@
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 (setopt display-fill-column-indicator-column 120)
 
+;; prefer splitting windows vertically
+(setq split-height-threshold nil)
+(setq split-width-threshold 0)
+
 (provide 'appearance)
 
 ;;; appearance.el ends here
