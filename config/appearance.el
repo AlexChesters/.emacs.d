550249415f1453af1d71b4efe7560afa49c1843a
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-11-28 10:16:08 +0000

    add zenburn

--- config/appearance.el
+++ config/appearance.el
@@ -1,4 +1,13 @@
 ;;; appearance.el --- Custom appearance settings
 
+;;; Commentary:
+
+;;; Code:
+
 (set-face-attribute 'default nil :height 150)
 (global-display-line-numbers-mode)
+(load-theme 'zenburn t)
+
+(provide 'appearance)
+
+;;; appearance.el ends here
