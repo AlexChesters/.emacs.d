2a7c0e144efe227299453326615c468ffd481214
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-11-28 10:17:53 +0000

    add ruler

--- config/appearance.el
+++ config/appearance.el
@@ -4,9 +4,18 @@
 
 ;;; Code:
 
+;; font size
 (set-face-attribute 'default nil :height 150)
+
+;; display line numbers
 (global-display-line-numbers-mode)
+
+;; theme
 (load-theme 'zenburn t)
+
+;; rulers
+(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+(setopt display-fill-column-indicator-column 120)
 
 (provide 'appearance)
 
