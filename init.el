8bc8b6558951311df29c0d51a3e7717f0e7f9a54
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-12-04 18:00:17 +0000

    change

--- init.el
+++ init.el
@@ -40,7 +40,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(tab-line ((t (:inherit variable-pitch :background "gray95" :foreground "black" :height 1.0)))))
 
 (provide 'init)
 
