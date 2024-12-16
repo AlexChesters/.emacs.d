11c5e2d59f8d284347d8fd3f5b5d3d95c1869755
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-12-16 10:14:54 +0000

    update melpa packages

---
+++ elpa/chatgpt-shell-20241213.2210/chatgpt-shell-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "chatgpt-shell" "20241213.2210"
+  "A multi-LLM shell (ChatGPT, Claude, Gemini, Kagi, Ollama, Perplexity) + editing integrations."
+  '((emacs       "28.1")
+    (shell-maker "0.74.1"))
+  :url "https://github.com/xenodium/chatgpt-shell"
+  :commit "4077031d1647436553b1e18a71875addd768103f"
+  :revdesc "4077031d1647")
