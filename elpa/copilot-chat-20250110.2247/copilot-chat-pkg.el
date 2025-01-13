a9f7f37d39e6b848895a0926dba5eb8eff36270f
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-01-13 12:53:54 +0000

    update packages

---
+++ elpa/copilot-chat-20250110.2247/copilot-chat-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "copilot-chat" "20250110.2247"
+  "Copilot chat interface."
+  '((request       "0.3.2")
+    (markdown-mode "2.6")
+    (emacs         "27.1")
+    (chatgpt-shell "1.6.1")
+    (magit         "4.0.0"))
+  :url "https://github.com/chep/copilot-chat.el"
+  :commit "51f63723205da3ef80da44321c34bad5283ee5d6"
+  :revdesc "51f63723205d"
+  :keywords '("convenience" "tools")
+  :authors '(("cedric.chepied" . "cedric.chepied@gmail.com"))
+  :maintainers '(("cedric.chepied" . "cedric.chepied@gmail.com")))
