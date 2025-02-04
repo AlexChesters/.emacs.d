f94b2033f64bd5ac4d4b481979f53efb6ef1e7db
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-02-04 10:50:31 +0000

    update melpa packages

---
+++ elpa/magit-20250203.2321/magit-pkg.el
@@ -0,0 +1,18 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit" "20250203.2321"
+  "A Git porcelain inside Emacs."
+  '((emacs         "27.1")
+    (compat        "30.0.2.0")
+    (llama         "0.6.0")
+    (magit-section "4.2.0")
+    (seq           "2.24")
+    (transient     "0.8.4")
+    (with-editor   "3.4.3"))
+  :url "https://github.com/magit/magit"
+  :commit "e959ab1057016fd1759b9b4a930eca8aa71828ef"
+  :revdesc "e959ab105701"
+  :keywords '("git" "tools" "vc")
+  :authors '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+             ("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
+                 ("Kyle Meyer" . "kyle@kyleam.com")))
---
+++ elpa/magit-section-20250203.1351/magit-section-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit-section" "20250203.1351"
+  "Sections for read-only buffers."
+  '((emacs  "27.1")
+    (compat "30.0.2.0")
+    (llama  "0.6.0")
+    (seq    "2.24"))
+  :url "https://github.com/magit/magit"
+  :commit "e40e8f199447bc8f121ad128ecd50017a595659f"
+  :revdesc "e40e8f199447"
+  :keywords '("tools")
+  :authors '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")))
