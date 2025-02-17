53bddbd77451c8787f285816b780f1cbf96ac406
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-02-17 10:09:43 +0000

    update melpa packages

---
+++ elpa/cfn-mode-20250209.904/cfn-mode-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "cfn-mode" "20250209.904"
+  "AWS cloudformation mode."
+  '((emacs     "27.0")
+    (f         "0.20.0")
+    (s         "1.12.0")
+    (yaml-mode "0.0.13"))
+  :url "https://gitlab.com/worr/cfn-mode"
+  :commit "a7d9a4914ed71ca4e79de37f11176b9fb253b846"
+  :revdesc "a7d9a4914ed7"
+  :keywords '("convenience" "languages" "tools")
+  :authors '(("William Orr" . "will@worrbase.com"))
+  :maintainers '(("William Orr" . "will@worrbase.com")))
