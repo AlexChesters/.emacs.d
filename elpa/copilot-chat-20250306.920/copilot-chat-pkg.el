eae30538ac7d06ee955f9b6dccf7f15105a02927
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2025-03-07 10:50:40 +0000

    update melpa packages

---
+++ elpa/copilot-chat-20250306.920/copilot-chat-pkg.el
@@ -0,0 +1,17 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "copilot-chat" "20250306.920"
+  "Copilot chat interface."
+  '((request       "0.3.2")
+    (markdown-mode "2.6")
+    (emacs         "27.1")
+    (magit         "4.0.0")
+    (transient     "0.8.3")
+    (org           "9.4.6")
+    (polymode      "0.2.2")
+    (shell-maker   "0.76.2"))
+  :url "https://github.com/chep/copilot-chat.el"
+  :commit "8a00deeeea9fdd897d3e31840c03a778eb775c7f"
+  :revdesc "8a00deeeea9f"
+  :keywords '("convenience" "tools")
+  :authors '(("cedric.chepied" . "cedric.chepied@gmail.com"))
+  :maintainers '(("cedric.chepied" . "cedric.chepied@gmail.com")))
---
+++ elpa/dumb-jump-20250305.1646/dumb-jump-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dumb-jump" "20250305.1646"
+  "Jump to definition for 50+ languages without configuration."
+  '((emacs "24.3")
+    (s     "1.11.0")
+    (dash  "2.9.0")
+    (popup "0.5.3"))
+  :url "https://github.com/jacktasia/dumb-jump"
+  :commit "f65725420114215330ab01456c034f3e6ee25cd6"
+  :revdesc "f65725420114"
+  :keywords '("programming"))
