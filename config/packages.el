4d112d549ec73e62d14c396adaa247f211a51a46
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2026-04-13 09:40:45 +0100

    fix flycheck

--- config/packages.el
+++ config/packages.el
@@ -100,6 +100,18 @@
   :hook
   (prog-mode . flycheck-mode)
   :config
+  (flycheck-define-checker cfn-lint
+    "A Cloudformation linter using cfn-python-lint."
+    :command ("cfn-lint" "-f" "parseable" source)
+    :error-patterns
+    ((warning line-start
+              (file-name) ":" line ":" column
+              ":" (one-or-more digit) ":" (one-or-more digit) ":"
+              (id "W" (one-or-more digit)) ":" (message) line-end)
+     (error line-start (file-name) ":" line ":" column
+            ":" (one-or-more digit) ":" (one-or-more digit) ":"
+            (id "E" (one-or-more digit)) ":" (message) line-end))
+    :modes (cfn-mode))
   (add-to-list 'flycheck-checkers 'cfn-lint))
 
 ;; magict
