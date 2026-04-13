64e5c972130f89d9aa61737f7c4f91fe7f07850e
Alex Chesters <267334359+AlexChestersBBC@users.noreply.github.com>
2026-04-13 11:49:50 +0100

    add bind

--- config/keys.el
+++ config/keys.el
@@ -32,6 +32,7 @@
 (global-set-key (kbd "s-<right>") 'tab-next) ; Cmd+right, Right tab
 (global-set-key (kbd "s-W") 'tab-close) ; Cmd+W, Close tab
 (global-set-key (kbd "s-b") 'ibuffer) ; Cmd+b, ibuffer
+(global-set-key (kbd "s-r") 'switch-to-buffer) ; Cmd+r, switch buffer
 (global-set-key (kbd "s-t") 'atc/toggle-vterm) ; Cmd+t, toggle terminal
 
 (provide 'keys)
